8b785baa24bf9237b90409c7532d19dbbe9ba00b
danshkk <shkurkodan@gmail.com>
2021-04-12 00:00:31 +0300

    first

---
+++ src/blocks/header/Header.js
@@ -0,0 +1,23 @@
+import s from './Header.module.scss';
+import logo from '../../assets/image/logo.svg'
+import user from '../../assets/image/user.png'
+
+function Header() {
+  return (
+    <header className={s.header}>
+      <div className={s.container}>
+        <div className={s.inner}>
+          <div className={s.wrapper}>
+            <img src={logo} alt="HELYFLY" className={s.logo} />
+            <a href="tel: +8(800)-123-12-12" className={s.phone}>Москва</a>
+          </div>
+          <a className={s.user} href="#" >
+            <img src={user} alt="" className={s.userImg} />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
