665f4bde52bdffe6f068f240710476d9d02a82b7
danshkk <shkurkodan@gmail.com>
2021-04-28 18:04:18 +0300

    fin

--- src/blocks/plan/Plan.js
+++ src/blocks/plan/Plan.js
@@ -1,46 +1,77 @@
 import s from './Plan.module.scss';
 import avatar from '../../assets/image/aboutAvatar.png'
+import Slider from '../../components/slider/Slider';
 
 function Plan() {
   return (
     <>
       <div className={s.containerBg}>
-          <div className={s.wrapper}>
-            <div className={s.plans}>
-                <div className={s.item}>
-                  <div className={s.circle}>1</div>
-                  <div className={s.inner}>
-                    <span className={s.title}>Где встречаемся?</span>
-                    <span>Москва</span>
-                  </div>
-                </div>
-                <div className={s.item}>
-                  <div className={s.circle}>2</div>
-                  <div className={s.inner}>
-                    <span className={s.title}>Время съемки</span>
-                    <span className={s.subtitle}>3 часа</span>
-                  </div>
-                </div>
-                <div className={s.item}>
-                  <div className={s.circle}>3</div>
-                  <div className={s.inner}>
-                    <span className={s.title}>Мест осталось</span>
-                    <span className={s.subtitle}>2 места</span>
-                  </div>
-                </div>
-                <div className={s.item}>
-                  <img src={avatar} alt=""/>
-                  <div className={s.inner}>
-                    <span className={s.name}>Мария Иванова</span>
-                    <span className={s.respons}>Фотограф</span>
-                    <a className={s.link} href="">Показать больше информации</a>
-                  </div>
-                </div>
+        <div className={s.wrapper}>
+          <div className={s.plans}>
+            <div className={s.item}>
+              <div className={s.circle}>1</div>
+              <div className={s.inner}>
+                <span className={s.title}>Где встречаемся?</span>
+                <span>Москва</span>
+              </div>
+            </div>
+            <div className={s.item}>
+              <div className={s.circle}>2</div>
+              <div className={s.inner}>
+                <span className={s.title}>Время съемки</span>
+                <span className={s.subtitle}>3 часа</span>
+              </div>
+            </div>
+            <div className={s.item}>
+              <div className={s.circle}>3</div>
+              <div className={s.inner}>
+                <span className={s.title}>Мест осталось</span>
+                <span className={s.subtitle}>2 места</span>
+              </div>
+            </div>
+            <div className={s.item}>
+              <img src={avatar} alt="" />
+              <div className={s.inner}>
+                <span className={s.name}>Мария Иванова</span>
+                <span className={s.respons}>Фотограф</span>
+                <a className={s.link} href="">Показать больше информации</a>
+              </div>
             </div>
           </div>
+        </div>
       </div>
-      <div className={s.cont}>
-        <div className={s.wrap}></div>
+      <div className={s.mobContainer}>
+        <div className={s.mobPlan}>
+          <div className={s.mobItem}>
+            <div className={s.circle}>1</div>
+            <div className={s.inner}>
+              <span className={s.title}>Где встречаемся?</span>
+              <span>Москва</span>
+            </div>
+          </div>
+          <div className={s.mobItem}>
+              <div className={s.circle}>2</div>
+              <div className={s.inner}>
+                <span className={s.title}>Время съемки</span>
+                <span className={s.subtitle}>3 часа</span>
+              </div>
+            </div>
+            <div className={s.mobItem}>
+              <div className={s.circle}>3</div>
+              <div className={s.inner}>
+                <span className={s.title}>Мест осталось</span>
+                <span className={s.subtitle}>2 места</span>
+              </div>
+            </div>
+            <div className={s.mobItem}>
+              <img className={s.avatar} src={avatar} alt="" />
+              <div className={s.inner}>
+                <span className={s.name}>Мария Иванова</span>
+                <span className={s.respons}>Фотограф</span>
+                <a className={s.link} href="">Показать больше информации</a>
+              </div>
+            </div>
+        </div>
       </div>
     </>
   );
--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,2 +1,34 @@
+import s from './Slider.scss';
+import React from 'react';
+import Swiper from 'react-id-swiper';
+import 'swiper/swiper-bundle';
+import 'swiper/components/navigation/navigation.scss';
 
-        
+const Slider = () => {
+  const params = {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  }
+  return (
+    <Swiper {...params}>
+      <div>Slide #1</div>
+      <div>Slide #2</div>
+      <div>Slide #3</div>
+      <div>Slide #4</div>
+      <div>Slide #5</div>
+    </Swiper>
+  )
+};
+export default Slider;
